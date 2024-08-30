0e73114c75f2a894ab7824ff0c1cf8e2a8fda175
Abdul <abdul@intodepth.in>
2024-08-31 00:57:38 +0530

    replacetag.sh modified

--- replacetag.sh
+++ replacetag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i "s/\(tag:[[:space:]]*\"v\)[0-9]\+\"/\1${BUILD_NUMBER}\"/" ./mavenwebapp/values.yaml
 
-sed -i 's/^version:.*/version: 0.1.${BUILD_NUMBER}/' ${CHART_NAME}/Chart.yaml
+sed -i "s/^version:.*/version: 0.1.${BUILD_NUMBER}/" ${CHART_NAME}/Chart.yaml
